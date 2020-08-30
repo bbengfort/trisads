f2a14d3872348455add208d947519e197296903c
Benjamin Bengfort <benjamin@bengfort.com>
2020-08-30 12:33:19 -0400

    very simple directory service UI

--- pb/pb.go
+++ pb/pb.go
@@ -1,3 +1,5 @@
 package pb
 
 //go:generate protoc -I . --go_out=plugins=grpc:. api.proto models.proto
+//go:generate protoc -I . --js_out=import_style=commonjs:../web/src/pb --grpc-web_out=import_style=commonjs,mode=grpcwebtext:../web/src/pb api.proto models.proto
+
